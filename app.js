65711f7c6f8b8c439eca29a088ed1a47bc647c45
robinbood <mercedesfarts@gmail.com>
2025-02-13 15:46:25 +0500

    prettier structure

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require("./utils/config")
+const logger = require("./utils/logger")
+const blogsRouter = require("./controllers/blogs")
+
+
+mongoose.set("strictQuery",false)
+
+
+
+mongoose.connect(config.mongoUrl).then(result => {
+    logger.info("connected to", config.mongoUrl);
+    }).catch(error => {
+        logger.error("error connecting to:", config.mongoUrl);
+        
+})
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/blogs",blogsRouter)
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+const mongoUrl =process.env.MONGO_URI
+
+module.exports = {PORT, mongoUrl}
