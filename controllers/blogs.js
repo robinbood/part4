0123376eb5b612807a4b526f1a95f198a5055047
robinbood <mercedesfarts@gmail.com>
2025-02-15 11:19:19 +0500

    part 4b done but a lil messed up

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,4 +12,16 @@ blogsRouter.get('/', async(request, response) => {
     const blogs = await Blog.find({})
     response.status(200).json(blogs)
 })
+
+blogsRouter.delete('/:id' , async(request,response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/', async(request, response) => {
+    const blog = new Blog(request.body)
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog,{new:true})
+    response.json(updatedBlog)
+} )
 module.exports=blogsRouter
